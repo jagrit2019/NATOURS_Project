d1ad4877917c14154e0cf76422691912fb3f41ed
JArit20 <jagritacharya2020@gmail.com>
2020-07-21 14:48:45 +0530

    refactored routing

--- app.js
+++ app.js
@@ -13,7 +13,71 @@ const port = 3000;
 
 const tours=JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 // getting sepcific tour using id in the url ":id" this is way we declear a variable
-app.get('/api/v1/tours/:id',(req,res)=>{
+
+
+
+const updateTour= (req,res)=>{
+    if(req.params.id= 1>tours.length){
+        return res.status(404).json({
+            status:'fail',
+            message:'Invalid ID'
+        });
+    }
+    res.status(201).json({
+        status:"success",
+        data:{
+            tour:'<Updated tour is here>'
+        }
+    })
+};
+
+   
+/////////////////Delete/////////////////////
+const deleteTour=(req,res)=>{
+    if(req.params.id= 1>tours.length){
+        return res.status(404).json({
+            status:'fail',
+            message:'Invalid ID'
+        });
+    }
+    res.status(204).json({
+        status:"success",
+        data: "null"
+        
+    })
+};
+
+//this is an event loop dont put a sync fucntion inside otherwise you are gone
+const getAllTours = (req,res)=>{
+    res.status(200).json({
+    status:"success",
+    results:tours.length,
+    data:{
+        tours
+    }
+    })
+};
+// app.get('/api/v1/tours',getAllTours);
+
+
+
+const createNew = (req,res)=>{
+    console.log(req.body);
+    const newId = tours[tours.length-1].id+1;
+    const newTour = Object.assign({id:newId},req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=>{
+        res.status(201).json({
+            status:'success',
+            data:{
+                tour:newTour
+            }
+        })
+    })
+    // res.send("DOne");
+}
+const createTour=(req,res)=>{
     //req.params that gives/to read the value of the variable
     console.log(req.params);
     // to converst string in to a number just multiply  with one
@@ -38,8 +102,33 @@ app.get('/api/v1/tours/:id',(req,res)=>{
              })
             
          }
-   
-    // if(id<=tours.length){
+}
+// app.get('/api/v1/tours/:id',createTour);
+// app.patch('/api/v1/tours/:id',updateTour);
+// app.delete('/api/v1/tours/:id',deleteTour);
+// app.post('/api/v1/tours',createNew);
+
+//best optimised
+app.route('/api/v1/tours')
+.get(getAllTours)
+.post(createNew);
+
+
+
+app.route('/api/v1/tours/:id')
+.get(createTour)
+.patch(updateTour)
+.delete(deleteTour);
+// app.get('/',(req,res)=>{
+// res.status(200).json({message:'Hello form the server side', app:'Natours'})
+// })
+// app.post('/',(req,res)=>{
+//     res.send("This is the post request")
+// })
+app.listen(port,()=>{
+    console.log(`the server is running on ${port}`);
+});
+ // if(id<=tours.length){
     //      const tour = tours.find(el=>el.id===id)
     // res.status(200).json({
     //     status:"success",
@@ -59,85 +148,7 @@ app.get('/api/v1/tours/:id',(req,res)=>{
        
     // }
    
-    });
+    
 
   // put we expect an entire new uodate object
   //patch properties that should are been updated
-
-    app.patch('/api/v1/tours/:id',(req,res)=>{
-        if(req.params.id= 1>tours.length){
-            return res.status(404).json({
-                status:'fail',
-                message:'Invalid ID'
-            });
-        }
-        res.status(201).json({
-            status:"success",
-            data:{
-                tour:'<Updated tour is here>'
-            }
-        })
-    });
-//this is an event loop dont put a sync fucntion inside otherwise you are gone
-app.get('/api/v1/tours',(req,res)=>{
-res.status(200).json({
-    status:"success",
-    results:tours.length,
-    data:{
-        tours
-    }
-})
-});
-/////////////////Delete/////////////////////
-
-app.delete('/api/v1/tours/:id',(req,res)=>{
-    if(req.params.id= 1>tours.length){
-        return res.status(404).json({
-            status:'fail',
-            message:'Invalid ID'
-        });
-    }
-    res.status(204).json({
-        status:"success",
-        data: "null"
-        
-    })
-});
-//this is an event loop dont put a sync fucntion inside otherwise you are gone
-app.get('/api/v1/tours',(req,res)=>{
-res.status(200).json({
-status:"success",
-results:tours.length,
-data:{
-    tours
-}
-})
-});
-app.post('/api/v1/tours',(req,res)=>{
-    console.log(req.body);
-    const newId = tours[tours.length-1].id+1;
-    const newTour = Object.assign({id:newId},req.body);
-
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err=>{
-        res.status(201).json({
-            status:'success',
-            data:{
-                tour:newTour
-            }
-        })
-    })
-    // res.send("DOne");
-});
-
-
-
-// app.get('/',(req,res)=>{
-// res.status(200).json({message:'Hello form the server side', app:'Natours'})
-// })
-// app.post('/',(req,res)=>{
-//     res.send("This is the post request")
-// })
-app.listen(port,()=>{
-    console.log(`the server is running on ${port}`);
-});
