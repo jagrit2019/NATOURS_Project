6ea46cbe8ccdd59944b189e77f8028901758f67d
JArit20 <jagritacharya2020@gmail.com>
2020-07-21 18:26:31 +0530

    original

--- server.js
+++
@@ -1,9 +0,0 @@
-import { config } from 'dotenv';
-import { listen } from './app';
-
-config({ path: './config.env' });
-
-const port = process.env.PORT || 3000;
-listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
