5483c1f1f1f227f5c55df93845121b7fab492ebd
JArit20 <jagritacharya2020@gmail.com>
2020-07-22 00:32:39 +0530

    better file structure

---
+++ mongodb.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const MongoClient = require('mongodb').MongoClient;
+
+// replace the uri string with your connection string.
+const DB= "mongodb+srv://jagrit:Jha8875091601@cluster0.w9uya.mongodb.net/text?retryWrites=true&w=majority"
+
+mongoose.connect(DB,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false
+}).then(con=>{
+    console.log(con.connected);
+    console.log('DB connection successfull')
+})
+
+// MongoClient.connect(DB, function(err, client) {
+//    if(err) {
+//         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+//    }
+//    console.log('Connected...');
+//    const collection = client.db("test").collection("devices");
+//    // perform actions on the collection object
+//    client.close();
+// });
--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-// /* eslint-disable node/no-missing-require */
-// // eslint-disable-next-line node/no-missing-require
-// // eslint-disable-next-line import/no-unresolved
-// const dotenv = require('dotenv');
-// const app = require('./app');
+const dotenv = require('dotenv');
+const app = require('./app');
 
-// dotenv.config({ path: './config.env' });
+dotenv.config({ path: './config.env' });
 
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`App running on port ${port}...`);
-// });
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
