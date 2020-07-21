15567b67e6f67ba8d563ca701447b3867926e706
JArit20 <jagritacharya2020@gmail.com>
2020-07-21 13:03:34 +0530

    patch implimented

--- app.js
+++ app.js
@@ -14,20 +14,70 @@ const port = 3000;
 const tours=JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 // getting sepcific tour using id in the url ":id" this is way we declear a variable
 app.get('/api/v1/tours/:id',(req,res)=>{
-    //req.params that gives the value of the variable
+    //req.params that gives/to read the value of the variable
     console.log(req.params);
     // to converst string in to a number just multiply  with one
+    
     const id = req.params.id *1;
-    const tour = tours.find(el=>el.id===id)
-    res.status(200).json({
-        status:"success",
+
+   
+         const tour = tours.find(el=>el.id===id)
+         if(!tour){
+            res.status(404).json({
+                status:"fail",
+                message:"Sorry invalid id"
+             })
+         }
+         else{
+            res.status(200).json({
+                status:"success",
+              
+                data:{
+                    tour
+                }
+             })
+            
+         }
+   
+    // if(id<=tours.length){
+    //      const tour = tours.find(el=>el.id===id)
+    // res.status(200).json({
+    //     status:"success",
       
-        data:{
-            tour
-        }
-     })
+    //     data:{
+    //         tour
+    //     }
+    //  })
+    // }
+    // else{
+    //     res.status(404).json({
+    //         status:"fail",
+    //         message:"Sorry invalid id"
+          
+       
+    //      })
+       
+    // }
+   
     });
 
+  // put we expect an entire new uodate object
+  //patch properties that should are been updated
+
+    app.patch('/api/v1/tours/:id',(req,res)=>{
+        if(req.params.id= 1>tours.length){
+            return res.status(404).json({
+                status:'fail',
+                message:'Invalid ID'
+            });
+        }
+        res.status(200).json({
+            status:"success",
+            data:{
+                tour:'<Updated tour is here>'
+            }
+        })
+    });
 //this is an event loop dont put a sync fucntion inside otherwise you are gone
 app.get('/api/v1/tours',(req,res)=>{
 res.status(200).json({
