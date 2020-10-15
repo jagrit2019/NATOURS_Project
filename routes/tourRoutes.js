00a965df644b88ed840539f29b25835a9a6a6eb1
vsharma366 <61411275+vsharma366@users.noreply.github.com>
2020-10-15 19:47:04 +0530

    Update tourRoutes.js

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,7 +4,8 @@ const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
-
+// the code will only run if you import the express modules as these all functionalites are possible just because of it
+// it would be really messy if you didnt have used it
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
