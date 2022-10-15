7b4506381e8819e0c50656f5e4c6b9b4de98d9bf
nansharma <61489196+nansharma@users.noreply.github.com>
2022-10-15 19:28:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 
 
-const app = express();
-
-// 1) MIDDLEWARES
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
