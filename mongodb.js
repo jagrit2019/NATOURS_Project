0ccbaac9614839fd7f85578a8a6243e352c8c7c4
bacharya2020 <73686508+bacharya2020@users.noreply.github.com>
2022-10-24 09:28:43 +0530

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,4 @@
 
-// dotenv.config.env.DATABASE.replace('<Pass>',process.env)
-// MongoClient.connect(DB, function(err, client) {
 //    if(err) {
 //         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
 //    }
